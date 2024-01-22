455f829554d21db58df125ac335d138c695fb6fc
ValeriGuerrero <valeriguerrero22@outlook.com>
2024-01-22 17:50:02 -0600

    <Final project>

--- js/index.js
+++ js/index.js
@@ -27,6 +27,25 @@ copyright.innerHTML = "© Valeri Guerrero" + thisYear;
 
 // Using "DOM Manipulation", append the 'copyright' element to the 'footer'
 footer.appendChild(copyright);
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 
 //creating a variable listing my skills
 let skills = ["Javascript: Beginner", "CSS: Intermediate", "HTML: Intermediate"];
